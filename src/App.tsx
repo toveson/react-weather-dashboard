1657ebfa4dbd1f299fa4f026fb5be17b2811f87b
Trever O <toveson9522@gmail.com>
2023-10-11 22:38:02 -0600

    removed a div that isn't needed from App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import {Dashboard} from "./views"
 function App() {
   return (
     <div style={{display: "flex", flexDirection: "column", height: "100vh"}}>
-      <div style={{flex: 1, overflow: "auto"}}>
-        <Dashboard />
-      </div>
+      <Dashboard />
     </div>
   )
 }

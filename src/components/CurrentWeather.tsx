821db10cc98074a6bb4819250c35fccc8df1bc68
Trever O <toveson9522@gmail.com>
2023-10-13 23:58:23 -0600

    dispalying some current weather data

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,28 +1,31 @@
-import ConstructionIcon from "@mui/icons-material/Construction"
 import {useContext, useEffect, useState} from "react"
 import WeatherContext from "../WeatherContext"
+import {Stack, Typography} from "@mui/material"
+
+interface CurrentData {
+  temp: number
+  humidity: number
+  wind_speed: number
+  uvi: number
+}
 
 export const CurrentWeather: React.FC = () => {
   const {currentWeatherData} = useContext(WeatherContext)
-  const [currentData, setCurrentData] = useState({})
+  const [currentData, setCurrentData] = useState<CurrentData>()
 
   useEffect(() => {
     if (currentWeatherData !== undefined) {
-      setCurrentData(currentWeatherData?.current)
+      setCurrentData(currentWeatherData?.current as CurrentData)
     }
   }, [currentWeatherData])
 
   console.log(currentData)
   return (
-    <>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        <ConstructionIcon
-          style={{
-            fontSize: 400
-          }}
-        />
-      </div>
-      <h1> CurrentWeather are currently under construction</h1>
-    </>
+    <Stack>
+      <Typography>temperature: {currentData?.temp}</Typography>
+      <Typography>Humidity: {currentData?.humidity}</Typography>
+      <Typography>Wind Speed: {currentData?.wind_speed}</Typography>
+      <Typography>UV Index: {currentData?.uvi}</Typography>
+    </Stack>
   )
 }
